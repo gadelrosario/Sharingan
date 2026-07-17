c074d7d11d0504cab6ce86ac6e03066787c1990b
gadelrosario <gerardadelrosario@me.com>
2026-07-16 20:12:15 -0700

    Chunin Reforged 2.0

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,4 @@
-const CACHE='gfh-chunin-1-3-1';
-const STATIC=['./','./index.html','./manifest.webmanifest','./icons/icon.svg'];
-self.addEventListener('install',event=>{
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(STATIC)).then(()=>self.skipWaiting()));
-});
-self.addEventListener('activate',event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
-});
-self.addEventListener('fetch',event=>{
-  const url=new URL(event.request.url);
-  if(url.pathname.endsWith('/data/players.json') || url.pathname.endsWith('/index.html') || url.pathname.endsWith('/')){
-    event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{
-      const copy=response.clone();
-      caches.open(CACHE).then(cache=>cache.put(event.request,copy));
-      return response;
-    }).catch(()=>caches.match(event.request)));
-    return;
-  }
-  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)));
-});
+const CACHE='fantasy-hq-reforged-v2';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./data/players.json','./icons/icon.svg']))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
